4e1c8485f0f2a7d12275c6d05fb6a7eb96f19abd
sunxiaobin <1206775291@qq.com>
2018-11-19 12:11:17 +0800

    note

--- note.cmd
+++ note.cmd
@@ -4,6 +4,10 @@ $ git add filesname 提交文件（文件夹）到暂存区
 
 $ git commit -m "注释" 将缓存区内容添加到本地仓库
 
+$ git reset --hard HEAD^ 将版本回退到上一个版本
+
+$ git reset --hard  版本号前几（5）位 将版本跳转到指定版本
+
 $ git status 查看状态
 
 $ git log    查看提交
